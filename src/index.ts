b119816b2a3866296be073378b1f86c4a8099f76
Steven Lewis <steven.c.lewis3@gmail.com>
2021-03-22 12:52:54 -0400

    Replaced .forEach loops with for(...of...) loops, for async functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { update } from 'lodash';
-import _ = require('lodash');
+import * as _ from 'lodash';
 import { SpaceTraders } from 'spacetraders-sdk';
-import { Cargo, Marketplace, YourShip, Good, LocationResponse, LocationsResponse, MarketplaceResponse, AccountResponse, User, SellResponse, PurchaseResponse, FlightPlanResponse } from 'spacetraders-sdk/dist/types';
+import { Cargo, Marketplace, YourShip, LocationsResponse, MarketplaceResponse, AccountResponse, User, SellResponse, PurchaseResponse } from 'spacetraders-sdk/dist/types';
 
 export interface LoadedShip {
     ship: YourShip,
@@ -36,21 +35,21 @@ async function main() {
         (e) => {console.log(e);});
         currentUser = (userResponse as AccountResponse).user;
         if(_.isEmpty(currentShips)) {
-            (userResponse as AccountResponse).user.ships.forEach((ship) => {
+            for (const ship of (userResponse as AccountResponse).user.ships) {
                 let addShip: LoadedShip = {ship, cargoCost: 0};
                 currentShips.push(addShip);
-            });
+            }
         } else {
-            (userResponse as AccountResponse).user.ships.forEach((ship) => {
+            for (const ship of (userResponse as AccountResponse).user.ships) {
                 let updateShip = currentShips.find((cship) => { return cship.ship.id === ship.id});
                 if (updateShip) {
                     (updateShip as LoadedShip).ship = ship;
                 } else {
                     currentShips.push({ ship, cargoCost: 0 });
                 }
-            })
+            }
         }
-        currentShips.forEach(async (cargoShip) => {
+        for (const cargoShip of currentShips) {
             currentShip = cargoShip;
             let shipLoc = cargoShip.ship.location;
             console.log("Current Credits: "+(userResponse as AccountResponse).user.credits);
@@ -61,7 +60,7 @@ async function main() {
             } else {
                 console.log("Ship "+cargoShip.ship.id+" currently in transit");
             }
-        });
+        }
         await delay(10000);
     }
 }
@@ -78,7 +77,7 @@ async function doSomething() {
 
 async function sellGoods(stationMarket: Marketplace[]) {
     if (currentShip.ship.cargo.length > 0) {
-        currentShip.ship.cargo.forEach(async (item) => {
+        for (const item of currentShip.ship.cargo) {
             if (item.good === "FUEL") {
                 const order = await spaceTraders.sellGood(currentShip.ship.id, "FUEL", item.quantity);
                 currentShip.ship = (order as SellResponse).ship;
@@ -92,14 +91,13 @@ async function sellGoods(stationMarket: Marketplace[]) {
                         (e) => {
                             console.log(e);
                         });
-                        console.log(order);
                         currentShip.ship = (order as SellResponse).ship;
                         currentShips[0].ship = (order as SellResponse).ship;
                         console.log("Ship "+currentShip.ship.id+" selling "+item.good+" for "+(order as SellResponse).order.total);
                     }
                 }
             }
-        })
+        }
     }
 }
 
@@ -109,7 +107,7 @@ async function updateMarketData(location: string) {
         console.log(e);
     });
     console.log("Updating Market data");
-    (marketData as MarketplaceResponse).location.marketplace.forEach((item) => {
+    for (const item of (marketData as MarketplaceResponse).location.marketplace) {
         if (item.symbol !== "FUEL") {
             let updateItem = marketGoods.find((good) => { return good.symbol === item.symbol});
             const updateIndex = marketGoods.indexOf(updateItem as Goods);
@@ -133,40 +131,44 @@ async function updateMarketData(location: string) {
                 });
             }
         }
-    });
+    }
     return (marketData as MarketplaceResponse).location.marketplace;
 }
 
 async function buyGoods(stationMarket: Marketplace[]) {
     // 20 used for fuel considerations.
     let goodsAvailable: PurchaseGoods[] = [];
-    stationMarket.forEach(async (good) => {
+    for (const good of stationMarket) {
         const itemData = marketGoods.find((item) => { return good.symbol === item.symbol});
         if (itemData) {
             goodsAvailable.push({good, gainPerQty: itemData.highPrice - good.pricePerUnit});
         }
-    });
+    }
     goodsAvailable = _.sortBy(goodsAvailable, ['gainPerQty']);
     goodsAvailable = _.reverse(goodsAvailable);
 
-    const good: Marketplace = goodsAvailable[0].good;
-    if (currentShip.ship.spaceAvailable > 20 && currentShip.ship.cargo.length === 0) {
-        let quantityToBuy = Math.floor((currentShip.ship.spaceAvailable - 20) / good.volumePerUnit);
-        if (good.volumePerUnit === 0) {
-            quantityToBuy = Math.floor((currentUser.credits - 500) / good.pricePerUnit);
+    for (const purchaseGood of goodsAvailable) {
+        const good: Marketplace = purchaseGood.good;
+        if (currentShip.ship.spaceAvailable > 20 && purchaseGood.gainPerQty > 0 && currentUser.credits >= good.pricePerUnit) {
+            let quantityToBuy = Math.floor((currentShip.ship.spaceAvailable - 20) / good.volumePerUnit);
+            if (good.volumePerUnit === 0) {
+                quantityToBuy = Math.floor((currentUser.credits - 500) / good.pricePerUnit);
+            }
+            if (quantityToBuy > good.quantityAvailable) {
+                quantityToBuy = good.quantityAvailable;
+            }
+            if (quantityToBuy * good.pricePerUnit > currentUser.credits) {
+                quantityToBuy = Math.floor(currentUser.credits / good.pricePerUnit);
+            }
+            currentShip.ship.spaceAvailable -= quantityToBuy * good.volumePerUnit;
+            console.log("Ship "+currentShip.ship.id+" buying "+good.symbol+" for "+good.pricePerUnit * quantityToBuy);
+            await spaceTraders.purchaseGood(currentShip.ship.id, good.symbol, quantityToBuy).then((d) => { 
+                currentShip.ship = d.ship;
+                currentUser.credits = d.credits;
+                return d; 
+            }, 
+            (e) => {console.log(e);});
         }
-        if (quantityToBuy > good.quantityAvailable) {
-            quantityToBuy = good.quantityAvailable;
-        }
-        if (quantityToBuy * good.pricePerUnit > currentUser.credits) {
-            quantityToBuy = Math.floor(currentUser.credits / good.pricePerUnit);
-        }
-        currentShip.ship.spaceAvailable -= quantityToBuy * good.volumePerUnit;
-        console.log("Ship "+currentShip.ship.id+" buying "+good.symbol+" for "+good.pricePerUnit * quantityToBuy);
-        const order = await spaceTraders.purchaseGood(currentShip.ship.id, good.symbol, quantityToBuy).then((d) => { return d; }, 
-        (e) => {console.log(e);});
-        currentUser.credits = (order as PurchaseResponse).credits;
-        currentShip.ship = (order as PurchaseResponse).ship;
     }
 }
 
@@ -223,7 +225,7 @@ async function backupNavigation(systemLocs: LocationsResponse) {
     let shortestTripDist = 100;
     const currentLoc = systemLocs.locations.find((loc) => { return loc.symbol === currentShip.ship.location});
     if (currentLoc) {
-        systemLocs.locations.forEach((loc) => {
+        for (const loc of systemLocs.locations) {
             if (loc.symbol !== currentLoc.symbol) {
                 const distToLoc = distance(currentLoc.x, loc.x, currentLoc.y, loc.y);
                 if (distToLoc < shortestTripDist) {
@@ -231,7 +233,7 @@ async function backupNavigation(systemLocs: LocationsResponse) {
                     shortestTrip = loc.symbol;
                 }
             }
-        });
+        }
         const targetLoc = systemLocs.locations.find((loc) => { return loc.symbol === shortestTrip});
         if (targetLoc) {
 
